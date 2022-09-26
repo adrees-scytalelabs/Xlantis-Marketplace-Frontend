3910e756d95c7c873c87ae1fd937d89b197265f0
Touseef Ali <touseef.ali@scytalelabs.com>
2022-09-26 17:32:31 +0500

    Addresses changed for collecible1155 and factory1155 after

--- src/app/components/blockchain/Addresses/Addresses.js
+++ src/app/components/blockchain/Addresses/Addresses.js
@@ -3,8 +3,8 @@ export const CreateCubeAddress = '0x2fC35aEDBC3d47837F24A746D67782fe9c87C0E8';
 export const AuctionAddress = "0x21A6256e5BB6Fe4f83F76A9701402ed555bDCFac";
 export const WethAddress = "0x0a180a76e4466bf68a7f86fb029bed3cccfaaac5";
 export const MarketPlaceAddress = "0x89c91582cD0098449E066eB5A31fC10986eACBB1";
-export const Collectible1155Address = "0x101cF077b88FF5a158CAc1dc66731Ef2397758Ea";
-export const Factory1155Address = "0xEFf4019554F0c41a8aaf774299E6a3B4786208da";
+export const Collectible1155Address = "0x969BeE61c3DDa4B3555e796Dc4d778cBD243F53D";
+export const Factory1155Address = "0x0C71F769F45b12dC8421730229730B36EA4Ab508";
 export const Collectible721Address = "0x7717a1b5FEC2d7e22E1d2e54EA17AAd0358B6EB3";
 export const Factory721Address = "0xA343276B0F853918de9BC2DEc2DcEb2B9F6214AE";
 
