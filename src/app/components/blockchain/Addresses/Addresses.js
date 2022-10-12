de38c3a4b83649d560c5e30e92cbe9d19020174f
Touseef Ali <touseef.ali@scytalelabs.com>
2022-10-13 00:24:41 +0500

    Abi and address added for Auction Drop Factory contract

--- src/app/components/blockchain/Addresses/Addresses.js
+++ src/app/components/blockchain/Addresses/Addresses.js
@@ -8,4 +8,5 @@ export const Factory1155Address = "0x3669aFdA9B50e69D8a495A3B3Fa91B25eF4C9843";
 export const Collectible721Address = "0xa89468f2dc966C997d9bFE974568eC9F34766211";
 export const Factory721Address = "0x769a0C6B02529Fb6CCC57bfAe1d30272bFc6AD57";
 export const FactoryDrop = "0x61259982727eDbc02d9eb08a29A3c87d14da0571";
+export const AuctionDropFactory = "0x7Aafa46D1b978180f3f053f8dd577C0f1BCe3531"
 
