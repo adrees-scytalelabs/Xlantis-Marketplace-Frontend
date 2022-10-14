74135a3511541d54063d5db593b94cfd98009414
ashba-scytalelabs <you@example.com>
2022-10-14 23:40:48 +0500

    Marketplace drops and nfts pages added
    added pages for market place auction and fixed-price drops and nfts

---
+++ src/app/containers/Pages/Dashboard/Admin/DropNfts.js
@@ -0,0 +1,235 @@
+import { Grid } from '@material-ui/core/';
+import TablePagination from '@material-ui/core/TablePagination';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import Card from '@material-ui/core/Card';
+import Web3 from 'web3';
+
+import { useLocation } from 'react-router-dom';
+import {
+    CardContent,
+    CardMedia,
+} from '@material-ui/core/';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import { makeStyles } from '@material-ui/core/styles';
+import { CardHeader } from 'reactstrap';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '100%', // 16:9
+    },
+    badge: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+}));
+
+function DropNfts(props) {
+    let location = useLocation();
+    const classes = useStyles();
+    const [rowsPerPage, setRowsPerPage] = React.useState(8);
+    const [totalNfts, setTotalNfts] = React.useState(0);
+    const [page, setPage] = React.useState(0);
+    const [tokenList, setTokenList] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const [nftIds, setNftIds] = useState([]);
+    let [isSaving, setIsSaving] = useState(false);
+    const [network, setNetwork] = useState("");
+    const [showNetworkModal, setShowNetworkModal] = useState(false);
+
+    
+    let [nftDetail, setNftDetail] = useState({});
+   
+
+    
+
+    
+    const handleCloseBackdrop = () => {
+        setOpen(false);
+    };
+    const handleShowBackdrop = () => {
+        setOpen(true);
+    };
+    let getNFTs = (start, end) => {
+        handleShowBackdrop();
+        console.log("nftids", location.state.nftId);
+        console.log("dropId", location.state.dropId);
+
+        let data = {
+            nftIds : location.state.nftId
+        }
+        axios.get(`/drop/nfts/${location.state.dropId}/${start}/${end}`, data).then(
+            (response) => {
+                console.log("response", response);
+                setTokenList(response.data.data);
+                console.log("title",response.data.data[0].title);
+                setTotalNfts(response.data.data.length);
+
+                handleCloseBackdrop();
+            },
+            (error) => {
+                if (process.env.NODE_ENV === "development") {
+                    console.log(error);
+                    console.log(error.response);
+                }
+                if (error.response.data !== undefined) {
+                    if (error.response.data === "Unauthorized access (invalid token) !!") {
+                        Cookies.remove("Authorization");
+                        localStorage.removeItem("Address")
+                        window.location.reload();
+                    }
+                }
+                handleCloseBackdrop();
+            })
+    }
+
+    useEffect(() => {
+
+        setNftIds(location.state.nftId);
+        getNFTs(0, rowsPerPage);
+        // getCollections();?
+
+        props.setActiveTab({
+            dashboard: "",
+            newNFT: "",
+            orders: "",
+            myNFTs: "",
+            myCubes: "",
+            myDrops: "",
+            settings: "",
+            mySeason: "",
+            privacyPolicy: "",
+            termsandconditions: "",
+            changePassword: "",
+            newDrop: "",
+            newCube: "",
+            newCollection: "",
+            newRandomDrop: "",
+        });// eslint-disable-next-line
+    }, []);
+    const handleChangePage = (event, newPage) => {
+        console.log("newPage", newPage);
+        setPage(newPage);
+        console.log("Start", newPage * rowsPerPage);
+        console.log("End", newPage * rowsPerPage + rowsPerPage);
+        getNFTs(newPage * rowsPerPage, newPage * rowsPerPage + rowsPerPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        getNFTs(0, parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    return (
+        <div className="card">
+            <ul className="breadcrumb" style={{ backgroundColor: "rgb(167,0,0)" }}>
+                <li className="breadcrumb-item">
+                    <a href="/">Dashboard</a>
+                </li>
+                <li className="breadcrumb-item active">Drop Nfts</li>
+            </ul>
+            <div className="card-body">
+                <form >
+                    <div className="form-group">
+
+                        {open ? (
+                            <div align="center" className="text-center">
+                                <Spinner
+                                    animation="border"
+                                    role="status"
+                                    style={{ color: "#ff0000" }}
+                                >
+                                </Spinner>
+                                <span style={{ color: "#ff0000" }} className="sr-only">Loading...</span>
+                            </div>
+                        ) : tokenList.length === 0 ? (
+                            <Card variant="outlined" style={{ padding: "40px", marginTop: '20px', marginBottom: '20px' }}>
+                                <Typography variant="body2" className="text-center" color="textSecondary" component="p"  >
+                                    <strong>No items to display </strong>
+                                </Typography>
+                            </Card>
+                        ) : (
+                            <Grid
+                                container
+                                spacing={2}
+                                direction="row"
+                                justify="flex-start"
+                            >
+                                {tokenList.map((i, index) => (
+                                    
+                                <Grid item xs={12} sm={6} md={3} key={index}>
+                                        <Card style={{ height: "100%" }} variant="outlined" className={classes.root}>
+                                            <CardActionArea>
+                                                <CardHeader className="text-center"
+                                                    title={i.title}
+                                                />
+                                                <CardMedia
+                                                    className={classes.media}
+                                                    image={i.nftURI}
+                                                    title="Drop Image"
+                                                >
+                                                </CardMedia>
+                                                <CardContent>
+                                                    <Typography variant="body2" color="textSecondary" component="p">
+                                                        <strong>Token Rarity: </strong>{i.type}
+                                                    </Typography>
+                                                    <Typography variant="body2" color="textSecondary" component="p">
+                                                        <strong>Token Supply: </strong>{i.tokenSupply}
+                                                    </Typography>
+                                                    <Typography variant="body2" color="textSecondary" component="p">
+                                                        <strong>Description: </strong>{i.description}
+                                                    </Typography>
+
+                                                   
+                                                </CardContent>
+                                            </CardActionArea>
+                                            
+                                        </Card>
+                                </Grid >
+                            ))}
+                            </Grid>
+                        )}
+                    </div>
+                    <TablePagination
+                        rowsPerPageOptions={[4, 8, 12, 24]}
+                        component="div"
+                        count={totalNfts}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onChangePage={handleChangePage}
+                        onChangeRowsPerPage={handleChangeRowsPerPage}
+                    />
+                </form>
+            </div >
+           
+        </div >
+
+    );
+}
+
+export default DropNfts;
+
