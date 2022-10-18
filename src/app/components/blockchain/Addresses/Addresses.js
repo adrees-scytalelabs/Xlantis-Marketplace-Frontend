8c771e580c1e562909379e3c2b9f175edfddfcee
Touseef Ali <touseef.ali@scytalelabs.com>
2022-10-18 22:45:47 +0500

    Abi and address for AuctionFactoryDrop changed because of updation

--- src/app/components/blockchain/Addresses/Addresses.js
+++ src/app/components/blockchain/Addresses/Addresses.js
@@ -8,5 +8,5 @@ export const Factory1155Address = "0x3669aFdA9B50e69D8a495A3B3Fa91B25eF4C9843";
 export const Collectible721Address = "0xa89468f2dc966C997d9bFE974568eC9F34766211";
 export const Factory721Address = "0x769a0C6B02529Fb6CCC57bfAe1d30272bFc6AD57";
 export const FactoryDrop = "0x59B34A2964450eEC9F44EC128B76304f1797Dd4f";
-export const AuctionDropFactory = "0x7Aafa46D1b978180f3f053f8dd577C0f1BCe3531"
+export const AuctionDropFactory = "0x6E6fB1Db7863670EBc007b496DF80a05D15a9B9B"
 
