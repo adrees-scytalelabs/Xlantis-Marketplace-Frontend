c53d911d262fe99c75ba4420ea84247c430c2595
touseef ali <touseef.ali@scytalelabs.com>
2023-03-26 23:34:13 +0500

    Banner image loader showing continuously issue solved

--- src/app/containers/Pages/Dashboard/Admin/NewDrop.js
+++ src/app/containers/Pages/Dashboard/Admin/NewDrop.js
@@ -525,55 +525,58 @@ function NewDrop(props) {
   };
 
   let onChangeBannerFile = async (e) => {
-    console.log("In banner change function");
-    setIsUploadingBanner(true);
-    const reader = new window.FileReader();
-    let imageNFT = e.target.files[0];
-    setImageType(e.target.files[0].type.split("/")[1]);
-    console.log("e.target.files[0]", e.target.files[0]);
-    // console.log("Image type: ", imageType);
-    reader.readAsArrayBuffer(e.target.files[0]);
-    reader.onloadend = () => {
-      console.log("reader.result", reader.result);
-      // setBuffer(Buffer(reader.result));
-      ipfs.add(Buffer(reader.result), async (err, result) => {
-        if (err) {
-          console.log("err", err);
+    console.log("In banner change function: ", e.target.files[0]);
+    const file = e.target.files[0];
+    if (file) {
+      setIsUploadingBanner(true);
+      const reader = new window.FileReader();
+      let imageNFT = e.target.files[0];
+      setImageType(e.target.files[0].type.split("/")[1]);
+      console.log("e.target.files[0]", e.target.files[0]);
+      // console.log("Image type: ", imageType);
+      reader.readAsArrayBuffer(e.target.files[0]);
+      reader.onloadend = () => {
+        console.log("reader.result", reader.result);
+        // setBuffer(Buffer(reader.result));
+        ipfs.add(Buffer(reader.result), async (err, result) => {
+          if (err) {
+            console.log("err", err);
+            setIsUploadingBanner(false);
+            let variant = "error";
+            enqueueSnackbar("Unable to Upload Image to IPFS ", { variant });
+            return;
+          }
+          console.log("HASH", result[0].hash);
+
+          setIpfsHash(result[0].hash);
+          setIpfsURI(`https://ipfs.io/ipfs/${result[0].hash}`);
+          let variant = "success";
+          enqueueSnackbar("Image Uploaded to IPFS", { variant });
+          //
+        });
+      };
+      // setIsUploadingIPFS(true);
+      let fileData = new FormData();
+      fileData.append("image", imageNFT);
+      axios.post(`/upload/image`, fileData).then(
+        (response) => {
+          console.log("response", response);
+          setBannerImage(response.data.url);
+          setIsUploadingBanner(false);
+          let variant = "success";
+          enqueueSnackbar("Image Uploaded Successfully", { variant });
+        },
+        (error) => {
+          if (process.env.NODE_ENV === "development") {
+            console.log(error);
+            console.log(error.response);
+          }
           setIsUploadingBanner(false);
           let variant = "error";
-          enqueueSnackbar("Unable to Upload Image to IPFS ", { variant });
-          return;
+          enqueueSnackbar("Unable to Upload Image", { variant });
         }
-        console.log("HASH", result[0].hash);
-
-        setIpfsHash(result[0].hash);
-        setIpfsURI(`https://ipfs.io/ipfs/${result[0].hash}`);
-        let variant = "success";
-        enqueueSnackbar("Image Uploaded to IPFS", { variant });
-        //
-      });
-    };
-    // setIsUploadingIPFS(true);
-    let fileData = new FormData();
-    fileData.append("image", imageNFT);
-    axios.post(`/upload/image`, fileData).then(
-      (response) => {
-        console.log("response", response);
-        setBannerImage(response.data.url);
-        setIsUploadingBanner(false);
-        let variant = "success";
-        enqueueSnackbar("Image Uploaded Successfully", { variant });
-      },
-      (error) => {
-        if (process.env.NODE_ENV === "development") {
-          console.log(error);
-          console.log(error.response);
-        }
-        setIsUploadingBanner(false);
-        let variant = "error";
-        enqueueSnackbar("Unable to Upload Image", { variant });
-      }
-    );
+      );
+    }
   };
 
   let onChangeFile = (e) => {
