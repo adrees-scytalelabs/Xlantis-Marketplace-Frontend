a1a1a491980a31890c7480e354db641a226c370c
kirmanshahab-scytalelabs <kirman.shahab@scytalelabs.com>
2023-04-13 00:02:24 +0500

    Updated myCollection api call to redux

---
+++ src/app/redux/getMyCollectionSlice.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import Cookies from "js-cookie";
+
+const initialState = {
+  collectionData:[],
+  collectionCont:8,
+};
+
+export const getMyCollection = createAsyncThunk(
+  'myCollection/getMyCollection',
+  async (name,thunkAPI) => {
+    try {
+
+      const resp = await axios(`/collection/myCollections/${name.start}/${name.end}`);
+      console.log("reduxResp",resp);
+
+      return resp.data;
+    } catch (error) {
+        console.log(error.response.data);
+        if (error.response.data !== undefined) {
+          if (
+            error.response.data === "Unauthorized access (invalid token) !!"
+          ) {
+            sessionStorage.removeItem("Authorization");
+            sessionStorage.removeItem("Address");
+            Cookies.remove("Version");
+
+            window.location.reload(false);
+          }
+        }
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  }
+);
+
+const getMyCollectionSlice = createSlice({
+  name: 'myCollection',
+  initialState,
+  reducers: {},
+  extraReducers:{
+      [getMyCollection.fulfilled]: (state, action) => {
+        state.collectionData = action.payload.collectionData;
+        state.collectionCont = action.payload.collectionCount;
+      },
+      [getMyCollection.rejected]: (state, action) => {
+        console.log(action);
+      }
+  },
+});
+
+
+
+export default getMyCollectionSlice.reducer;
+
+
--- src/store.js
+++ src/store.js
@@ -3,10 +3,12 @@ import countReducer from './app/redux/getCountSlice';
 
 import userCountReducer from './app/redux/getUserCount';
 import UserProfileReducer from './app/redux/getUserProfileSlice';
+import getMyCollectionReducer from './app/redux/getMyCollectionSlice';
 export const store = configureStore({
   reducer: {
     count:countReducer,
     userCount:userCountReducer,
-    userProfile:UserProfileReducer
+    userProfile:UserProfileReducer,
+    MyCollection:getMyCollectionReducer
   },
 });
