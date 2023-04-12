e54aa2c93169d159a5c4383b0f1cb0629eae9866
kirmanshahab-scytalelabs <kirman.shahab@scytalelabs.com>
2023-04-12 16:34:06 +0500

    Updated user profile api call to redux

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import countReducer from './app/redux/getCountSlice';
 
 import userCountReducer from './app/redux/getUserCount';
+import UserProfileReducer from './app/redux/getUserProfileSlice';
 export const store = configureStore({
   reducer: {
     count:countReducer,
-    userCount:userCountReducer
+    userCount:userCountReducer,
+    userProfile:UserProfileReducer
   },
 });
