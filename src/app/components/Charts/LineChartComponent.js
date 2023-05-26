abf6f1fb86c1b2459c26683589ed7dd2e349fc06
touseef ali <touseef.ali@scytalelabs.com>
2023-05-26 11:25:38 +0500

    UI completed for admin earnings screen

---
+++ src/app/components/Charts/LineChartComponent.js
@@ -0,0 +1,63 @@
+import { ColorType, createChart } from "lightweight-charts";
+import React, { useEffect, useRef } from "react";
+
+const LineChartComponent = (props) => {
+  const {
+    data,
+    colors: {
+      backgroundColor = "black",
+      lineColor = "#2962FF",
+      textColor = "white",
+      areaTopColor = "#2962FF",
+      areaBottomColor = "rgba(41, 98, 255, 0.28)",
+    } = {},
+  } = props;
+
+  const chartContainerRef = useRef();
+
+  useEffect(() => {
+    const handleResize = () => {
+      chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+    };
+
+    const chart = createChart(chartContainerRef.current, {
+      layout: {
+        background: { type: ColorType.Solid, color: backgroundColor },
+        textColor,
+      },
+      width: chartContainerRef.current.clientWidth,
+      height: 300,
+      grid: {
+        // vertLines: false,
+        horzLines: false,
+      },
+    });
+    chart.timeScale().fitContent();
+
+    const newSeries = chart.addAreaSeries({
+      lineColor,
+      topColor: areaTopColor,
+      bottomColor: areaBottomColor,
+    });
+    newSeries.setData(data);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+
+      chart.remove();
+    };
+  }, [
+    data,
+    backgroundColor,
+    lineColor,
+    textColor,
+    areaTopColor,
+    areaBottomColor,
+  ]);
+
+  return <div ref={chartContainerRef} />;
+};
+
+export default LineChartComponent;
--- src/app/containers/Pages/Dashboard/Admin/AdminEarnings.js
+++ src/app/containers/Pages/Dashboard/Admin/AdminEarnings.js
@@ -1,9 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import EarningsImage from "../../../../assets/img/Earnings.png";
+import EarningsImage from "../../../../assets/img/EarningsImage.png";
+import { Grid } from "@mui/material";
+import LineChartComponent from "../../../../components/Charts/LineChartComponent";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import { getMaticBalance } from "../../../../components/API/AxiosInterceptor";
 
 const AdminEarnings = (props) => {
+  const [balance, setBalance] = useState(0);
+
+  const initialData = [
+    { time: "2018-12-22", value: 32.51 },
+    { time: "2018-12-23", value: 31.11 },
+    { time: "2018-12-24", value: 27.02 },
+    { time: "2018-12-25", value: 27.32 },
+    { time: "2018-12-26", value: 25.17 },
+    { time: "2018-12-27", value: 28.89 },
+    { time: "2018-12-28", value: 25.46 },
+    { time: "2018-12-29", value: 23.92 },
+    { time: "2018-12-30", value: 22.68 },
+    { time: "2018-12-31", value: 22.67 },
+  ];
+
+  const getBalance = () => {
+    getMaticBalance()
+      .then((response) => {
+        setBalance(response.data?.maticBalance);
+      })
+      .catch((error) => {
+        console.log("Error from getting matic balance: ", error);
+      });
+  };
+
   useEffect(() => {
+    getBalance();
     props.setActiveTab({
       dashboard: "",
       earnings: "active",
@@ -37,33 +67,161 @@ const AdminEarnings = (props) => {
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-5 bg-white text-dark">
-          <div className="row">
-            <div className="col-2">
-              <img src={EarningsImage} />
-            </div>
-            <div className="col-10 d-flex flex-column justify-content-end align-items-end">
-              <div>
-                <h1 className="col">
-                  <span style={{ fontFamily: "Orbitron" }}>$1000</span>
-                </h1>
+      <div className="m-2">
+        <div className="row">
+          <Grid container spacing={0}>
+            <Grid item xs={12} sm={12} md={10} lg={10} xl={10}>
+              <div
+                className="text-white border border-white"
+                style={{ backgroundColor: "#262626" }}
+              >
+                <div className="row p-3">
+                  <div className="col-4">
+                    <img className="h-auto w-auto" src={EarningsImage} />
+                  </div>
+                  <div className="col-8 d-flex flex-column justify-content-end align-items-end">
+                    <div>
+                      <h1 className="col">
+                        <span style={{ fontFamily: "Orbitron" }}>
+                          ${balance.toFixed(4)}
+                        </span>
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 className="col">
+                        <span
+                          style={{ fontFamily: "Inter" }}
+                          className="font-weight-light"
+                        >
+                          Current Balance
+                        </span>
+                      </h1>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div>
-                <h1 className="col">
-                  <span
-                    style={{ fontFamily: "Inter" }}
-                    className="font-weight-light"
-                  >
-                    Current Balance
-                  </span>
-                </h1>
-              </div>
-            </div>
-          </div>
+            </Grid>
+          </Grid>
         </div>
-        <div className="col-3 border border-white ml-1 h-80">Box 2</div>
-        <div className="col-3 border border-white ml-1 h-80">Box 3</div>
+        <div className="row mt-4">
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={12} md={5} lg={5} xl={5}>
+              <div className="border border-white">
+                <div className="row">
+                  <div className="col-4 d-flex justify-content-center align-items-center">
+                    <TrendingUpIcon fontSize="large" className="h-75 w-75" />
+                  </div>
+                  <div className="col-8">
+                    <div className="col-12 d-flex flex-column justify-content-end align-items-end">
+                      <div>
+                        <h1 className="col">
+                          <span
+                            style={{ fontFamily: "Orbitron" }}
+                            className="text-xl text-white font-weight-bold"
+                          >
+                            $1500
+                          </span>
+                        </h1>
+                      </div>
+                      <div>
+                        <h1 className="col">
+                          <span
+                            style={{ fontFamily: "Inter" }}
+                            className="font-weight-light text-xl"
+                          >
+                            Royalties Earned
+                          </span>
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={12} md={5} lg={5} xl={5}>
+              <div className="border border-white">
+                <div className="row">
+                  <div className="col-4 d-flex justify-content-center align-items-center">
+                    <TrendingUpIcon
+                      // fontSize="large"
+                      className="h-75 w-75"
+                    />
+                  </div>
+                  <div className="col-8">
+                    <div className="col-12 d-flex flex-column justify-content-end align-items-end">
+                      <div>
+                        <h1 className="col">
+                          <span
+                            style={{ fontFamily: "Orbitron" }}
+                            className="text-xl text-white font-weight-bold"
+                          >
+                            $1500
+                          </span>
+                        </h1>
+                      </div>
+                      <div>
+                        <h1 className="col">
+                          <span
+                            style={{ fontFamily: "Inter" }}
+                            className="font-weight-light text-xl"
+                          >
+                            NFT Earnings
+                          </span>
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+          <Grid container spacing={1} className="mt-2">
+            <Grid item xs={12} sm={12} md={10} lg={10} xl={10}>
+              <div className="border border-white">
+                <div className="row">
+                  <div className="col-2 d-flex justify-content-center align-items-center">
+                    <TrendingUpIcon fontSize="large" className="h-75 w-75" />
+                  </div>
+                  <div className="col-10 d-flex flex-column justify-content-end align-items-end">
+                    <div className="col-12 d-flex flex-column justify-content-end align-items-end">
+                      <div>
+                        <h1 className="col">
+                          <span
+                            style={{ fontFamily: "Orbitron" }}
+                            className="text-xl text-white font-weight-bold"
+                          >
+                            $1500
+                          </span>
+                        </h1>
+                      </div>
+                      <div>
+                        <h1 className="col">
+                          <span
+                            style={{ fontFamily: "Inter" }}
+                            className="font-weight-light text-xl"
+                          >
+                            Total Earnings
+                          </span>
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+          <Grid container spacing={1} className="mt-2">
+            <Grid item xs={12} sm={12} md={10} lg={10} xl={10}>
+              <div className="border border-white p-2">
+                <LineChartComponent
+                  {...props}
+                  data={initialData}
+                  className="p-2"
+                />
+              </div>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     </div>
   );
