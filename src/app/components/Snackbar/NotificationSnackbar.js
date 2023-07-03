3b4aa3bd5f8223901c367012a1b86d6f7745a3bc
Saad Majeed <saad.majeed@scytalelabs.com>
2023-07-03 12:47:44 +0500

    properties cannot be empty

--- src/app/components/Snackbar/NotificationSnackbar.js
+++ src/app/components/Snackbar/NotificationSnackbar.js
@@ -6,8 +6,8 @@ import React from 'react'
 
 function NotificationSnackbar(props) {
     return (
-        <Snackbar open={props.open} sx={{ zIndex: 9999 }} autoHideDuration={6000} onClose={props.handleClose}>
-            <Alert severity={props.severity} sx={{ width: '100%',zIndex: 9999 }}>
+        <Snackbar open={props.open} style={{ zIndex: 9999 }} autoHideDuration={6000} onClose={props.handleClose}>
+            <Alert severity={props.severity}  sx={{ width: '100%'}}>
                 {props.message}
             </Alert>
         </Snackbar>
