4524c25b56af92295537e8061e16c37a333846b7
adrees-scytalelabs <muhammad.adrees@scytalelabs.com>
2023-07-13 12:19:38 +0500

    installed charaUI and resolved its polyfil error of IPFS and @walletconnect

--- src/app/components/IPFS/ipfs.js
+++ src/app/components/IPFS/ipfs.js
@@ -1,13 +1,17 @@
-const IPFS = require('ipfs-api');
+import { create } from 'ipfs-http-client';
+
 const projectId = '2EPV5Le3s2sosAm3u7pJekyoE0u'; 
 const projectSecret = '238f5ea43aed9bd06186f54036a4e502'; 
 
 const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
 
-
-const ipfs = IPFS({
-    host: 'ipfs.infura.io', port: '5001', protocol: 'https', headers: {
-        authorization: auth,
-    },
+const ipfs = create({
+   host: 'ipfs.infura.io',
+   port: '5001',
+   protocol: 'https',
+   headers: {
+       authorization: auth,
+   },
 });
-export default ipfs
+
+export default ipfs;
